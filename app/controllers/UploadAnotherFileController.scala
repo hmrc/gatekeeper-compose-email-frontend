b27c3d883ed3158fc8f654ccd87c7b868d2701f7
srinivasalumunagala <srinivasalu.munagala@digital.hmrc.gov.uk>
2022-03-10 10:12:34 +0000

    APID-366-TakeThree 1st commit

---
+++ app/controllers/UploadAnotherFileController.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.mvc._
+import uk.gov.hmrc.govukfrontend.views.Aliases._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class UploadAnotherFileController @Inject() (
+  mcc: MessagesControllerComponents,
+  formProvider: UploadAnotherFileFormProvider,
+  view: UploadAnotherFileView,
+  implicit val ec: ExecutionContext
+) extends FrontendController(mcc)
+    with I18nSupport {
+
+  def onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    request.userAnswers.get(FileUploadPage).fold(
+      Future(Redirect(controllers.docUpload.routes.SupportingDocController.onLoad().url))
+    ) { files =>
+      if (files.isEmpty) {
+        Future.successful(Redirect(controllers.docUpload.routes.UploadFileController.onLoad()))
+      } else {
+        Future.successful(Ok(view(formProvider(), buildSummaryList(files), getOptionalDocs(request.userAnswers))))
+      }
+    }
+  }
+
+  def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    formProvider().bindFromRequest().fold(
+      formWithErrors => resultWithErrors(formWithErrors),
+      addAnotherFile => {
+        if (addAnotherFile) {
+          Future.successful(Redirect(controllers.docUpload.routes.UploadFileController.onLoad()))
+        } else {
+          if (request.checkMode) {
+            Future.successful(Redirect(controllers.cya.routes.CheckYourAnswersController.onLoad()))
+          } else {
+            Future.successful(Redirect(controllers.contactDetails.routes.DeclarantContactDetailsController.onLoad()))
+          }
+        }
+      }
+    )
+  }
+
+  private def resultWithErrors(
+    formWithErrors: Form[Boolean]
+  )(implicit request: DataRequest[AnyContent]): Future[Result] =
+    request.userAnswers.get(FileUploadPage).fold(
+      Future(Redirect(controllers.docUpload.routes.UploadFileController.onLoad().url))
+    ) { files =>
+      Future.successful(BadRequest(view(formWithErrors, buildSummaryList(files), getOptionalDocs(request.userAnswers))))
+    }
+
+  private def getOptionalDocs(userAnswers: UserAnswers): Seq[OptionalDocument] = {
+    val anyOptionalDocs = userAnswers.get(AnyOtherSupportingDocsPage).getOrElse(false)
+    if (anyOptionalDocs) {
+      userAnswers.get(OptionalSupportingDocsPage).getOrElse(Seq.empty)
+    } else {
+      Seq.empty
+    }
+  }
+
+  private def buildSummaryList(files: Seq[FileUploadInfo])(implicit messages: Messages) = {
+    val summaryListRows = files.zipWithIndex.map { case (file, index) =>
+      val removeLink = controllers.docUpload.routes.RemoveUploadedFileController.onLoad(Index(index)).url
+      SummaryListRow(
+        key = Key(content = Text(file.fileName), classes = s"govuk-!-width-one-third govuk-!-font-weight-regular".trim),
+        value = Value(
+          content = HtmlContent(
+            s"""<a href=$removeLink class="govuk-link"> <span aria-hidden="true">${messages("common.remove")}</span>
+               |<span class="govuk-visually-hidden"> Remove ${file.fileName}</span>
+               |</a>""".stripMargin
+          ),
+          classes = "govuk-summary-list__actions"
+        ),
+        actions = None
+      )
+    }
+    SummaryList(
+      classes = "govuk-!-margin-bottom-9",
+      rows = summaryListRows
+    )
+  }
+
+}
---
+++ app/models/upscan/FileUpload.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.upscan
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+import play.api.libs.json._
+
+import java.time.{Instant, LocalDateTime}
+import scala.util.Try
+
+case class FileUpload(
+  reference: String,
+  credId: Option[String] = None,
+  downloadUrl: Option[String] = None,
+  fileStatus: Option[FileStatusEnum] = None,
+  uploadDetails: Option[UploadDetails] = None,
+  failureDetails: Option[FailureDetails] = None,
+  lastUpdatedDate: Option[Instant] = None
+) {
+  val fileName = uploadDetails.map(_.fileName)
+}
+
+object FileUpload {
+
+  val callbackReads: Reads[FileUpload] = (
+    (JsPath \ "reference").read[String] and
+      (JsPath \ "credId").readNullable[String] and
+      (JsPath \ "downloadUrl").readNullable[String] and
+      (JsPath \ "fileStatus").readNullable[FileStatusEnum] and
+      (JsPath \ "uploadDetails").readNullable[UploadDetails] and
+      (JsPath \ "failureDetails").readNullable[FailureDetails] and
+      (JsPath \ "lastUpdatedDate").readNullable[Instant]
+  )(FileUpload.apply _)
+
+  implicit val format: OFormat[FileUpload] = Json.format[FileUpload]
+}
+
+case class UploadDetails(uploadTimestamp: LocalDateTime, checksum: String, fileName: String, fileMimeType: String)
+
+object UploadDetails {
+
+  implicit val formats: Format[UploadDetails] = Format(
+    ((JsPath \ "uploadTimestamp").read[LocalDateTime] and
+      (JsPath \ "checksum").read[String] and
+      (JsPath \ "fileName").read[String].map(decodeMimeEncodedWord) and
+      (JsPath \ "fileMimeType").read[String])(UploadDetails.apply _),
+    ((JsPath \ "uploadTimestamp").write[LocalDateTime] and
+      (JsPath \ "checksum").write[String] and
+      (JsPath \ "fileName").write[String] and
+      (JsPath \ "fileMimeType").write[String])(unlift(UploadDetails.unapply _))
+  )
+
+  def decodeMimeEncodedWord(word: String): String =
+    Try(MimeUtility.decodeText(word)).getOrElse(word)
+}
+
+case class FailureDetails(failureReason: String, message: String)
+
+object FailureDetails {
+
+  implicit val format: OFormat[FailureDetails] = Json.format[FailureDetails]
+}
